7c344aa94ff5fb987f0b185f019be7ca0c970d8e
mechack tambwe <mesutarso@users.noreply.github.com>
2021-11-02 00:23:44 +0100

    initial static test

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    tabWidth: 2,
+    printWidth: 80,
+    endOfLine: "lf",
+    arrowParens: 'avoid',
+    trailingComma: 'es5',
+    semi: true,
+    useTabs: false,
+    singleQuote: true,
+    bracketSpacing: true,
+    
+  
+  };
